62b9cf43f3c0d08a803cda36b196fb7f4f0abd30
rocketman9159 <rocket.man9159@gmail.com>
2023-02-27 21:59:59 -0500

    handlebars baseline maybe

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,9 +18,9 @@ router.get('/', async (req, res) => {
     const spring = springData.map((spring) => spring.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
+    res.render('homepage', { spring
+      // projects, 
+      // logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
