9637f4ea7f183a3f565a2273547c87a3d50bc3cc
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-03-02 13:25:53 -0500

    got some routes sneaking

--- public/js/script.js
+++ public/js/script.js
@@ -26,24 +26,24 @@ const petFriendlyM = document.getElementById("petfriendlym");
 // These variables have to be declared with let because the distance and weather icon will be updated
 
 
-let parks = [wekiwaSprings, silverSprings, rainbowSprings, rockSpringsRun, ginnieSprings, blueSpring, deLeonSprings, fanningSprings, manateeSprings, weekiWacheeSprings, itchetuckneeSprings, madisonSprings, royalSprings, bobsRiverPlace];
-let springList = ["Wekiwa Springs State Park", "Silver Springs State Park", "Rainbow Springs State Park", "Rock Springs Run State Reserve", "Ginnie Springs", "Blue Spring State Park", "DeLeon Springs State Park", "Fanning Springs State Park", "Manatee Springs State Park", "Weeki Wachee Springs State Park", "Ichetucknee Springs State Park", "Madison Springs", "Royal Springs", "Bobs River Place"];
+// let parks = [wekiwaSprings, silverSprings, rainbowSprings, rockSpringsRun, ginnieSprings, blueSpring, deLeonSprings, fanningSprings, manateeSprings, weekiWacheeSprings, itchetuckneeSprings, madisonSprings, royalSprings, bobsRiverPlace];
+// let springList = ["Wekiwa Springs State Park", "Silver Springs State Park", "Rainbow Springs State Park", "Rock Springs Run State Reserve", "Ginnie Springs", "Blue Spring State Park", "DeLeon Springs State Park", "Fanning Springs State Park", "Manatee Springs State Park", "Weeki Wachee Springs State Park", "Ichetucknee Springs State Park", "Madison Springs", "Royal Springs", "Bobs River Place"];
 
-function init() {
-  //For now are are using session storage to load all springs data into the users browser, this will not be needed once we have a database
-  sessionStorage.setItem(`parks`, JSON.stringify(parks));
-  for (let i = 0; i < parks.length; i++){
-    sessionStorage.setItem(parks[i].name, JSON.stringify(parks[i]))
-  }
-  populateCards();
- };
+// function init() {
+//   //For now are are using session storage to load all springs data into the users browser, this will not be needed once we have a database
+//   sessionStorage.setItem(`parks`, JSON.stringify(parks));
+//   for (let i = 0; i < parks.length; i++){
+//     sessionStorage.setItem(parks[i].name, JSON.stringify(parks[i]))
+//   }
+//   populateCards();
+//  };
 
 // ******************************************
-  // navigator.geolocation.getCurrentPosition(function(position) {
-  //   let userLoc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  //   let parksAsLatLng = parks.map(function(park) {
-  //     return new google.maps.LatLng(park.lat, park.lng);
-  //   });
+  navigator.geolocation.getCurrentPosition(function(position) {
+    let userLoc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    let parksAsLatLng = parks.map(function(park) {
+      return new google.maps.LatLng(park.lat, park.lng);
+    });
 
   
       // distanceSpanEl.classList = "distance-span";
@@ -87,56 +87,53 @@ function getweather(zipcode, imageEl) {
 // ******************************************
 // *******Search listener *******************
 // ******************************************
-submitEl.addEventListener("click", function(event) {
-  event.preventDefault;
+// submitEl.addEventListener("click", function(event) {
+//   event.preventDefault;
 
-  let userSP = stateParkEl.checked;
-  let userPet = petFriendlyEl.checked;
-  let userCamp = campingAllowedEl.checked;
-  let userScuba = scubaDivingEl.checked;
-  let userFee = pricingFeeEl.checked;
-  filterResults(userSP, userPet, userCamp, userScuba, userFee)
-});
+//   let userSP = stateParkEl.checked;
+//   let userPet = petFriendlyEl.checked;
+//   let userCamp = campingAllowedEl.checked;
+//   let userScuba = scubaDivingEl.checked;
+//   let userFee = pricingFeeEl.checked;
+//   filterResults(userSP, userPet, userCamp, userScuba, userFee)
+// });
 
 // ******************************************
 // *******Modal listeners *******************
 // ******************************************
-btnNew.addEventListener("click", function() {
-  modal.style.display = "block";
-});
-close.addEventListener("click", function() {
-  modal.style.display = "none";
-});
+// btnNew.addEventListener("click", function() {
+//   modal.style.display = "block";
+// });
+// close.addEventListener("click", function() {
+//   modal.style.display = "none";
+// });
 
-window.addEventListener("click", function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-});
+// window.addEventListener("click", function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// });
 
 // listener for the "add a new spring" button
-newSpring.addEventListener("click", function(event) {
-  event.preventDefault;
+// newSpring.addEventListener("click", function(event) {
+//   event.preventDefault;
  
-  let camp = "";
-  let pet = "'";
-  let spring = springNameM.value;
-  let desc = descriptionM.value;
-  if (campingM.checked) {
-    camp = "true";
-  } else {camp = false};
-  if (petFriendlyM.checked) {
-    pet = "true"
-  } else {pet = "false"};
-  let address = addressM.value;
+//   let camp = "";
+//   let pet = "'";
+//   let spring = springNameM.value;
+//   let desc = descriptionM.value;
+//   if (campingM.checked) {
+//     camp = "true";
+//   } else {camp = false};
+//   if (petFriendlyM.checked) {
+//     pet = "true"
+//   } else {pet = "false"};
+//   let address = addressM.value;
 
-  let newspring = [spring, desc, camp, pet, address];
-//puts new spring in sessionstorage, displays a new page asking for patience and notifying a server admin.  This will be changed once we have a database.
-  sessionStorage.setItem("newspring", JSON.stringify(newspring));
-  document.location.assign("./newspring.html");
-});
+//   let newspring = [spring, desc, camp, pet, address];
+// //puts new spring in sessionstorage, displays a new page asking for patience and notifying a server admin.  This will be changed once we have a database.
+//   sessionStorage.setItem("newspring", JSON.stringify(newspring));
+//   document.location.assign("./newspring.html");
+// });
 
-init();
-
-
-
+// init();
