49937427da56a2c0c5291b23283c34008d26111a
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-03-02 22:10:23 -0500

    got rid of model

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,8 +50,7 @@ router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: User }],
+      attributes: { exclude: ['password'] }
     });
 
     const user = userData.get({ plain: true });
