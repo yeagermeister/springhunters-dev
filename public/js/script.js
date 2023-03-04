22a1d2efa938f84213d05d2f5151b251b01187a0
unknown <johnyeager75@gmail.com>
2023-03-04 12:28:09 -0500

    weather icons are working on the springcards on main.  Backed out of the chagne to global.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,75 +1,102 @@
-Global vars
+// Global vars
+
+// This server var needs switched before deploying to heroku
 let server = "http://localhost:3001";
 // let server = "https://springhunters1.herokuapp.com";
 
-
+// ***************************************
+// ********  DOM elements  ***************
+// ***************************************
 const stateParkEl = document.querySelector('#statePark');
 const petFriendlyEl = document.querySelector('#petFriendly');
 const campingAllowedEl = document.querySelector('#campingAllowed');
 const scubaDivingEl = document.querySelector('#scubaDiving');
 const pricingFeeEl = document.querySelector('#pricingFee');
 const submitEl = document.querySelector('#searchBtn');
-// const distanceEl = document.querySelector()
 
+// ***************************************
+// ***********  Functions  ***************
+// ***************************************
 
+async function init() {
+  const springarray = await fetchsprings();
+    for (let i = 0; i < springarray.length; i++) {
+      let j = i + 1;
+      let distanceEl = document.querySelector(`#spring${j}`);
+      let lat = springarray[i].lat;
+      let lng = springarray[i].lng
+      getUserLoc(lat, lng, distanceEl);
+      let weatherEl = document.querySelector(`#weather${j}`);
+      let zipcode = springarray[i].zipcode;
+      getweather(zipcode, weatherEl)
+    }
+    getSprings();
+};
 
+// Get the springs from the database and populate the dropdown
+function getSprings() {
+  let conn = `${server}/api/springs/`;
+  const dropdownEl = document.querySelector("#dropdown");
+  fetch(conn)
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (data) {
+          for (let i = 0; i < data.length; i++) {
+              const anchor = document.createElement('a');
+              anchor.href = `${server}/springs/${data[i].id}`
+              anchor.innerText = `${data[i].name}`;
+              anchor.classList = "dropdown-item"
+              dropdownEl.appendChild(anchor);
+          }
+      });
+};
 
+  // Send a GET request to the RapidAPI weather API
+function getweather(zipcode, weatherEl) {    
+    const API_KEY = '4a9c9446f7msh1bdc5860de01184p135179jsne7c04d560051';
+    const API_HOST = 'weatherapi-com.p.rapidapi.com';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': API_HOST
+      }
+    };
+      //utilizes the zipcode from the static map array
+    fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${zipcode}`, options)
+      .then(response => response.json())
+      .then(data => {
+        // chooses which icon to display
+        const iconUrl = data.current.condition.icon;
+        let imageEl = document.createElement('img')
+        imageEl.setAttribute("src", iconUrl);
+        weatherEl.appendChild(imageEl);
+      })
+      //displays error message
+      .catch(err => console.error(err));
+};
 
-// function init() {
-//   //For now are are using session storage to load all springs data into the users browser, this will not be needed once we have a database
-//   sessionStorage.setItem(`parks`, JSON.stringify(parks));
-//   for (let i = 0; i < parks.length; i++){
-//     sessionStorage.setItem(parks[i].name, JSON.stringify(parks[i]))
-//   }
-//   populateCards();
-//  };
+// populates the springlist array for later use
+async function fetchsprings() {
+var requestSpringObjects = `${server}/api/springs/`;
+const springlist = await (await fetch(requestSpringObjects)).json();
+return springlist;
+};
 
-// ******************************************
-// function finduserloc(){
-//   navigator.geolocation.getCurrentPosition(function(position) {
-//     let userLoc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-//     console.log(userLoc);
-//   })};
-//   let parksAsLatLng = parks.map(function(park) {
-//     return new google.maps.LatLng(park.lat, park.lng);
-//   });
-
-
-// distanceSpanEl.classList = "distance-span";
-// distanceSpanEl.setAttribute("id", "distance-" + i);
-// //using google maps geography feature to map the distance between the users coordinates and the park displayed on the cards coordinates for display, and converting it to miles
-// let distanceInMeters = google.maps.geometry.spherical.computeDistanceBetween(userLoc, parksAsLatLng[i]);
-// let distanceInMiles = distanceInMeters / 1609.344;
-// let rounded = Math.round(distanceInMiles)
-// distanceSpanEl.textContent = rounded + ' miles away';
-
-// let weatherEl = document.createElement('span');
-// weatherEl.classList = "wicon"
-// weatherEl.setAttribute("id", "weather" + i)
-// let imageEl = document.createElement('img');
-
-// // //   Send a GET request to the RapidAPI weather API
-// // // function getweather(zipcode, imageEl) {    
-// // //   const API_KEY = '4a9c9446f7msh1bdc5860de01184p135179jsne7c04d560051';
-// // //   const API_HOST = 'weatherapi-com.p.rapidapi.com';
-// // //   const options = {
-// // //     method: 'GET',
-// // //     headers: {
-// // //       'X-RapidAPI-Key': API_KEY,
-// // //       'X-RapidAPI-Host': API_HOST
-// // //     }
-// // //   };
-// // //     //utilizes the zipcode from the static map array
-// // //   fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${zipcode}`, options)
-// // //     .then(response => response.json())
-// // //     .then(data => {
-// // //       const iconUrl = data.current.condition.icon;
-// // //       imageEl.setAttribute("src", iconUrl);
-// // //     })
-// // //     .catch(err => console.error(err));
-// // // };
-// //  dd
-// d
+// function to populate the distance and weather spans
+function getUserLoc(lat, lng, distanceEl) {
+navigator.geolocation.getCurrentPosition(position => {
+const userLat = position.coords.latitude;
+const userLng = position.coords.longitude;
+const userLatLng = new google.maps.LatLng(userLat, userLng);
+var springLatLng = new google.maps.LatLng(lat, lng);
+var distance = google.maps.geometry.spherical.computeDistanceBetween(userLatLng, springLatLng);
+let distanceInMiles = distance / 1609.344;
+let rounded = Math.round(distanceInMiles);
+distanceEl.innerHTML = rounded + ' miles away';
+})
+};
 
 // ******************************************
 // *******Search listener *******************
@@ -82,9 +109,9 @@ submitEl.addEventListener("click", async function(event) {
   let userCamp = campingAllowedEl.checked;
   let userScuba = scubaDivingEl.checked;
   let userFee = pricingFeeEl.checked;    
-  let url = "http://localhost:3001/";
+  let url = server;
   let i = 0;
-
+// build up the url to pass to the api
   if (userSP === true) {i = i + 1};
   if (userPet === true) {i = i + 1};
   if (userCamp === true) {i = i + 1};
@@ -116,38 +143,5 @@ submitEl.addEventListener("click", async function(event) {
   window.location.replace(url);
 });
 
-// populates the springlist array for later use
-async function fetchsprings() {
-  var requestSpringObjects = `${server}/api/springs/`;
-  const springlist = await (await fetch(requestSpringObjects)).json();
-  return springlist;
-};
-
-// function to get the list of springs from the database and populate the dropdown list
-
-  function getUserLoc(lat, lng, distanceEl) {
-    navigator.geolocation.getCurrentPosition(position => {
-      const userLat = position.coords.latitude;
-      const userLng = position.coords.longitude;
-      const userLatLng = new google.maps.LatLng(userLat, userLng);
-      var springLatLng = new google.maps.LatLng(lat, lng);
-      var distance = google.maps.geometry.spherical.computeDistanceBetween(userLatLng, springLatLng);
-      let distanceInMiles = distance / 1609.344;
-      let rounded = Math.round(distanceInMiles);
-      distanceEl.innerHTML = rounded + ' miles away';
-    })
-  }
-
-  async function init() {
-    const springarray = await fetchsprings();
-    for (let i = 0; i < springarray.length; i++) {
-      let j = i + 1;
-      let distanceEl = document.querySelector(`#spring${j}`);
-      let lat = springarray[i].lat;
-      let lng = springarray[i].lng
-      getUserLoc(lat, lng, distanceEl);
-    }
-    getsprings();
-  }
   init();
   
